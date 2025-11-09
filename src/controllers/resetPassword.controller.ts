f44973b70f93536db2889489aabeff8bef225bff
vidaHd <hd.vida4471@gmail.com>
2025-11-09 15:02:56 +0330

    add

--- src/controllers/resetPassword.controller.ts
+++ src/controllers/resetPassword.controller.ts
@@ -1,7 +1,5 @@
-// controllers/resetPassword.controller.ts
 import { Request, Response } from "express";
 import { User } from "../models/User";
-import { sendSMS } from "../helpers/sms.helper";
 import { sendEmail } from "../helpers/email.helper";
 import bcrypt from "bcrypt";
 
@@ -10,39 +8,42 @@ const verificationCodes: Record<string, string> = {};
 export const sendResetCode = async (req: Request, res: Response) => {
   const { mobileNumber } = req.body;
   const user = await User.findOne({ mobileNumber });
-  if (!user) return res.status(404).json({ message: "User not found" });
+  if (!user)
+    return res.status(404).json({ message: req.t("auth.user_not_found") });
 
   const code = Math.floor(1000 + Math.random() * 9000).toString();
   verificationCodes[mobileNumber] = code;
 
-  //   await sendSMS(mobileNumber, `Your verification code is ${code}`);
+  // await sendSMS(mobileNumber, req.t("auth.sms_code_message", { code }));
 
   await sendEmail(
     user.email,
     req.t("auth.verification_code_subject"),
-    `Your verification code is ${code}`
+    req.t("", { code })
   );
-  res.json({ message: "Verification code sent" });
+
+  res.json({ message: req.t("auth.verification_code_sent") });
 };
 
 export const verifyResetCode = async (req: Request, res: Response) => {
   const { mobileNumber, code } = req.body;
   if (verificationCodes[mobileNumber] === code) {
-    res.json({ message: "Code verified" });
+    res.json({ message: req.t("auth.code_verified") });
   } else {
-    res.status(400).json({ message: "Invalid code" });
+    res.status(400).json({ message: req.t("auth.invalid_code") });
   }
 };
 
 export const resetPassword = async (req: Request, res: Response) => {
   const { mobileNumber, newPassword } = req.body;
   const user = await User.findOne({ mobileNumber });
-  if (!user) return res.status(404).json({ message: "User not found" });
+  if (!user)
+    return res.status(404).json({ message: req.t("auth.user_not_found") });
 
   const hashed = await bcrypt.hash(newPassword, 10);
   user.password = hashed;
   await user.save();
 
   delete verificationCodes[mobileNumber];
-  res.json({ message: "Password reset successfully" });
+  res.json({ message: req.t("auth.password_reset_success") });
 };
