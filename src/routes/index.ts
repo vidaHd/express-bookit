8f80e007c0987ddead89a8f2bb73fa0da1476166
vidaHd <hd.vida4471@gmail.com>
2025-12-15 12:18:17 +0330

    fix

---
+++ src/routes/index.ts
@@ -0,0 +1,21 @@
+import authRoutes from "./auth.routes";
+import profileRoutes from "./profile.routes";
+import resetRoutes from "./reset.routes";
+import companyRoutes from "./company.routes";
+import serviceRoutes from "./service.routes";
+import companyServiceRoutes from "./company-service.routes";
+import jobRoutes from "./job.routes";
+import availableTimeRoutes from "./availableTime.routes";
+import bookingRoutes from "./booking.routes";
+
+export {
+  authRoutes,
+  profileRoutes,
+  resetRoutes,
+  companyRoutes,
+  serviceRoutes,
+  companyServiceRoutes,
+  jobRoutes,
+  availableTimeRoutes,
+  bookingRoutes,
+};
