40c0da920dcd0f31ad5c17c5ace07421e90ca8ae
vidaHd <hd.vida4471@gmail.com>
2025-12-23 09:46:39 +0330

    fix example migration

--- migrate-templates/migration.js
+++ migrate-templates/migration.js
@@ -1,17 +1,11 @@
 /**
- * Template Migration File for migrate-mongo (pnpm)
- * Copy this file to migrations/ and rename it for each migration
  * Example filename: 20251222-add-role-to-users.js
  */
 
 module.exports.up = async (db) => {
-  // TODO: تغییرات دیتابیس را اینجا اعمال کن
-  // مثال:
   // await db.collection("users").updateMany({}, { $set: { role: "user" } });
 };
 
 module.exports.down = async (db) => {
-  // TODO: تغییرات up را برگردان
-  // مثال:
   // await db.collection("users").updateMany({}, { $unset: { role: "" } });
 };
